4dc9bc4304ae4d950cd9b147924750813b6aa919
yeryery <ynopotblu@gmail.com>
2016-10-19 12:13:25 +0300

    Рефакторинг контроллера

--- src/ua/com/juja/yeryery/Controller.java
+++ src/ua/com/juja/yeryery/Controller.java
@@ -8,6 +8,8 @@ public class Controller {
 
     private View view;
     private Command[] commands;
+    String connectToDatabase = "Please, enter: " +
+                "'connect|database|username|password' or use command 'help'";
 
     public Controller(View view, DatabaseManager manager) {
         this.view = view;
@@ -22,36 +24,38 @@ public class Controller {
 
     public void run() {
         view.write("Hello, user!");
-        String connectToDatabase = "Please, enter: " +
-                "'connect|database|username|password' or use command 'help'";
         view.write(connectToDatabase);
 
         try {
-            while (true) {
-                String input = view.read();
-
-                for (Command command : commands) {
-                    try {
-                        if (command.canProcess(input)) {
-                            command.process(input);
-                            break;
-                        }
-                    } catch (Exception e) {
-                        if (e instanceof NullPointerException) {
-                            view.write(String.format("You can`t use '%s' unless you are not connected.", input));
-                            view.write(connectToDatabase);
-                            break;
-                        }
-                        if (e instanceof ExitException) {
-                            return;
-                        }
-                        printError(e);
-                        break;
-                    }
-                }
-            }
+            execute();
         } catch (ExitException e) {
 
+        }
+    }
+
+    private void execute() {
+        while (true) {
+            String input = view.read();
+
+            for (Command command : commands) {
+                try {
+                    if (command.canProcess(input)) {
+                        command.process(input);
+                        break;
+                    }
+                } catch (Exception e) {
+                    if (e instanceof NullPointerException) {
+                        view.write(String.format("You can`t use '%s' unless you are not connected.", input));
+                        view.write(connectToDatabase);
+                        break;
+                    }
+                    if (e instanceof ExitException) {
+                        return;
+                    }
+                    printError(e);
+                    break;
+                }
+            }
         }
     }
 
