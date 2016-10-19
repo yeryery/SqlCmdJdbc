966fae5852f25cd0d052b477422df0f861a23e46
yeryery <ynopotblu@gmail.com>
2016-10-19 17:07:17 +0300

    Метод для выбора таблицы проверяет введенное значение - является ли оно числом и существует ли таблица под соответствующим номером

--- src/ua/com/juja/yeryery/commands/dialogs/ChooseTable.java
+++ src/ua/com/juja/yeryery/commands/dialogs/ChooseTable.java
@@ -1,5 +1,6 @@
 package ua.com.juja.yeryery.commands.dialogs;
 
+import ua.com.juja.yeryery.commands.ExitException;
 import ua.com.juja.yeryery.manager.DatabaseManager;
 import ua.com.juja.yeryery.view.View;
 
@@ -8,14 +9,30 @@ public class ChooseTable implements Dialog {
     public String askUser(DatabaseManager manager, View view) {
         String[] tableNames = manager.getTableNames();
 
-        view.write("Please select number of table you need");
+        int size = 0;
+        int tableNumber = -1;
+        while (tableNumber < 0 || tableNumber > size) {
+            view.write("Please select number of table you need or '0' to exit");
 
-        int size = tableNames.length;
-        for (int i = 0; i < size; i++) {
-            view.write((i + 1) + " " + tableNames[i]);
+            size = tableNames.length;
+            for (int i = 0; i < size; i++) {
+                view.write((i + 1) + ". " + tableNames[i]);
+            }
+            view.write("0. Exit");
+
+            try {
+                tableNumber = Integer.parseInt(view.read());
+                if (tableNumber == 0) {
+                    view.write("See you!");
+                    throw new ExitException();
+                }
+                if (tableNumber < 0 || tableNumber > size) {
+                    view.write("There is no table with this number! Try again");
+                }
+            } catch (NumberFormatException e) {
+                view.write("This is not a number! Try again.");
+            }
         }
-
-        int tableNumber = Integer.parseInt(view.read());
 
         String tableName = "";
         for (int i = 0; i < size; i++) {
