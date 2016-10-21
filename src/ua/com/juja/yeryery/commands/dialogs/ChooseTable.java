767305b0468e4c684d6629eae1e909f544c02e22
yeryery <ynopotblu@gmail.com>
2016-10-21 15:17:42 +0300

    Реализована возможность выбора нужной таблицы двумя способами: ввод названия таблицы или выбор номера таблицы из списка доступных, также можно вернуться назад в меню

--- src/ua/com/juja/yeryery/commands/dialogs/ChooseTable.java
+++ src/ua/com/juja/yeryery/commands/dialogs/ChooseTable.java
@@ -1,45 +1,72 @@
 package ua.com.juja.yeryery.commands.dialogs;
 
-import ua.com.juja.yeryery.commands.ExitException;
 import ua.com.juja.yeryery.manager.DatabaseManager;
 import ua.com.juja.yeryery.view.View;
+
+import java.util.Arrays;
 
 public class ChooseTable implements Dialog {
     @Override
     public String askUser(DatabaseManager manager, View view) {
-        String[] tableNames = manager.getTableNames();
+        String[] names = manager.getTableNames();
 
         int size = 0;
         int tableNumber = -1;
-        while (tableNumber < 0 || tableNumber > size) {
-            view.write("Please select number of table you need or '0' to exit");
 
-            size = tableNames.length;
-            for (int i = 0; i < size; i++) {
-                view.write((i + 1) + ". " + tableNames[i]);
-            }
-            view.write("0. Exit");
 
-            try {
-                tableNumber = Integer.parseInt(view.read());
+        String tableName = "";
+
+        while (tableName.equals("")) {
+        view.write("Please enter the name or select number of table you need");
+
+        size = names.length;
+        Arrays.sort(names);
+        for (int i = 0; i < size; i++) {
+            view.write((i + 1) + ". " + names[i]);
+        }
+        view.write("0. Back (go back to the menu)");
+            String input = view.read();
+
+            if (isParsable(input)) {
+                tableNumber = Integer.parseInt(input);
+                String check = "";
                 if (tableNumber == 0) {
-                    view.write("See you!");
-                    throw new ExitException();
+                    check = "Back";
                 }
                 if (tableNumber < 0 || tableNumber > size) {
                     view.write("There is no table with this number! Try again.");
                 }
-            } catch (NumberFormatException e) {
-                view.write("This is not a number! Try again.");
-            }
-        }
 
-        String tableName = "";
-        for (int i = 0; i < size; i++) {
-            if (i == tableNumber - 1) {
-                tableName = tableNames[i];
+                for (int i = 0; i < size; i++) {
+                    if (i == tableNumber - 1) {
+                        check = names[i];
+                    }
+                }
+                tableName = check;
+            } else {
+                String check = "";
+                for (String name : names) {
+                    if (input.equals(name) || input.equals("Back")) {
+                        check = input;
+                    }
+                }
+                if (check.equals("")) {
+                    view.write("Table with name '" + input + "' doesn't exists. Try again.");
+                }
+                tableName = check;
             }
         }
         return tableName;
+    }
+
+
+    private boolean isParsable(String read) {
+        boolean parsable = true;
+        try {
+            Integer.parseInt(read);
+        } catch (NumberFormatException e) {
+            parsable = false;
+        }
+        return parsable;
     }
 }
