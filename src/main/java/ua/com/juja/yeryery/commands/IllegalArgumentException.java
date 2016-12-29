a4511a782887b6c0296b5d335641479f4e1f1196
yeryery <ynopotblu@gmail.com>
2016-12-29 23:27:46 +0300

    Добавлены классы IllegalArgumentException и ConnectException для корректной обработки исключений соотв изменения в тестах и небольшие изменения в исходящих сообщениях

---
+++ src/main/java/ua/com/juja/yeryery/commands/IllegalArgumentException.java
@@ -0,0 +1,18 @@
+package ua.com.juja.yeryery.commands;
+
+public class IllegalArgumentException extends RuntimeException {
+    private String message;
+
+    public IllegalArgumentException(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return String.format("Error! %s\nTry again", message);
+    }
+
+    public String getCutMessage() {
+        return message;
+    }
+}
+
