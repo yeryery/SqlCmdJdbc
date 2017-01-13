5b5005d5d2a4e9167fafc59a1c02d976c4d21732
yeryery <ynopotblu@gmail.com>
2017-01-13 15:09:36 +0300

    Создан новый класс для обработки случая, когда отсутствует JDBC Driver

---
+++ src/main/java/ua/com/juja/yeryery/model/JdbcDriverException.java
@@ -0,0 +1,7 @@
+package ua.com.juja.yeryery.model;
+
+public class JdbcDriverException extends RuntimeException {
+    public JdbcDriverException(String message) {
+        super(message);
+    }
+}
