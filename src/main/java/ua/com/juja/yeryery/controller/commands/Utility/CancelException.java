1960412e3e33a091e7b677f47e1a89770feb850c
yeryery <ynopotblu@gmail.com>
2017-01-23 16:31:47 +0300

    получение сообщения об ошибке добавлено внутрь класса

--- src/main/java/ua/com/juja/yeryery/controller/commands/Utility/CancelException.java
+++ src/main/java/ua/com/juja/yeryery/controller/commands/Utility/CancelException.java
@@ -1,4 +1,8 @@
 package ua.com.juja.yeryery.controller.commands.Utility;
 
 public class CancelException extends RuntimeException {
+
+    public String getMessage() {
+        return "Command execution is canceled";
+    }
 }
